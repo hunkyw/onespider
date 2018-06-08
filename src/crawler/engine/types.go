333d066555c65538b62184c4976d9d28bb1f1058
Hunk Yuan <hunkyw@163.com>
2018-06-09 02:31:07 +0800

    Initial commit

--- src/crawler/engine/types.go
+++ src/crawler/engine/types.go
@@ -2,7 +2,7 @@ package engine
 
 type Request struct {
 	Url        string
-	ParserFunc func([] byte) ParseResult
+	ParserFunc func([] byte ) ParseResult
 }
 
 type ParseResult struct {
--- src/crawler/zhenai/parser/citylist.go
+++ src/crawler/zhenai/parser/citylist.go
@@ -11,15 +11,19 @@ const cityListRe  = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>(
 func ParseCityList(contents []byte) engine.ParseResult{
 	re := regexp.MustCompile(cityListRe)
 	matches := re.FindAllSubmatch(contents,-1)
-
+	limit := 10
 	result := engine.ParseResult{}
-	for _,m := range matches {
-		result.Items = append(result.Items,string(m[2]))
-		result.Requests = append(result.Requests, engine.Request{
-			Url:string(m[1]),
-			ParserFunc:engine.NilParser,
-		})
 
+	for _,m := range matches {
+		result.Items = append(result.Items,"City " + string(m[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:          string(m[1]),
+			ParserFunc:   ParseCity,
+		})
+		limit --
+		if limit == 0 {
+			break
+		}
 	}
 	return result
 }
